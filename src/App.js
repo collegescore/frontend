01ae27149388b662b6bb09ba2a9906414d2fcca9
Danielle <dmaddo@uw.edu>
2026-01-15 15:08:08 -0800

    Removed filler typography on App

--- src/App.js
+++ src/App.js
@@ -13,47 +13,9 @@ function App() {
       }}
     >
       <Typography variant="h1" color="primary">
-        H1 long 
+        Welcome 
       </Typography>
-      <Typography variant="h2" color="primary">
-        H2
-      </Typography>
-      <Typography variant="h3" color="primary">
-        H3
-      </Typography>
-      <Typography variant="h4" color="primary">
-        H4
-      </Typography>
-      <Typography variant="h5" color="primary">
-        H5
-      </Typography>
-      <Typography variant="h6" color="primary">
-        H6
-      </Typography>
-      <Typography variant="subtitle1" color="primary">
-        subtitle1
-      </Typography>
-      <Typography variant="subtitle2" color="primary">
-        subtitle2
-      </Typography>
-      <Typography variant="body1" color="primary">
-        body1
-      </Typography>
-      <Typography variant="body2" color="primary">
-        body2
-      </Typography>
-      <Typography variant="button" color="primary">
-        button
-      </Typography>
-      <Typography variant="caption" color="primary">
-        caption
-      </Typography>
-      <Typography variant="overline" color="primary">
-        overline
-      </Typography>
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
+      
     </Box>
   );
 }
