6fa5effeccf9b45f232fb1efacffa6b50b69221c
Danielle <dmaddo@uw.edu>
2026-01-15 15:07:04 -0800

    Font and typography  updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 
 function App() {
   return (
     <Box
       sx={{
         display: "flex",
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
@@ -12,8 +13,47 @@ function App() {
       }}
     >
       <Typography variant="h1" color="primary">
-        Welcome
+        H1 long 
       </Typography>
+      <Typography variant="h2" color="primary">
+        H2
+      </Typography>
+      <Typography variant="h3" color="primary">
+        H3
+      </Typography>
+      <Typography variant="h4" color="primary">
+        H4
+      </Typography>
+      <Typography variant="h5" color="primary">
+        H5
+      </Typography>
+      <Typography variant="h6" color="primary">
+        H6
+      </Typography>
+      <Typography variant="subtitle1" color="primary">
+        subtitle1
+      </Typography>
+      <Typography variant="subtitle2" color="primary">
+        subtitle2
+      </Typography>
+      <Typography variant="body1" color="primary">
+        body1
+      </Typography>
+      <Typography variant="body2" color="primary">
+        body2
+      </Typography>
+      <Typography variant="button" color="primary">
+        button
+      </Typography>
+      <Typography variant="caption" color="primary">
+        caption
+      </Typography>
+      <Typography variant="overline" color="primary">
+        overline
+      </Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
     </Box>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import InterWoff2 from './fonts/Inter-VariableFont_wght.woff2'
 
 const theme = createTheme({
   palette: {
@@ -7,9 +8,28 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: "Arial, Helvetica, sans-serif",
+    fontFamily: "'Inter', sans-serif",
+    h1:{
+      fontWeight: 700,
+      textTransform: "capitalize",
+    },
     button: {
       textTransform: "none",
+      fontSize: "1rem",
+      fontWeight: 600,
+    },
+  },
+  components:{
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Inter';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 100 900;
+          src: local('Inter'), local('Inter-Regular'), url(${InterWoff2}) format('woff2');
+        }
+      `,
     },
   },
 });
