d89ef3a1b5ee61909da162b6baf5edb564da58f0
Danielle <dmaddo@uw.edu>
2026-01-30 18:03:15 -0800

    paragraph styling that can be used globally. body1 with set color and margins that can be overriden if needed.

---
+++ src/components/common/Paragraph.tsx
@@ -0,0 +1,5 @@
+import { Typography, TypographyProps } from "@mui/material";
+
+export default function Paragraph(props: TypographyProps) {
+  return <Typography variant="body1" color="grayscale.main" gutterBottom sx={{ mb: 2 }} {...props} />;
+}
