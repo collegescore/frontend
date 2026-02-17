ab3c97df9c07f960e77075879fda9122a7646935
jackiecosa <jackie@benincosa.com>
2026-02-17 15:25:45 -0800

    format

--- src/components/search/Hero.tsx
+++ src/components/search/Hero.tsx
@@ -1,21 +1,20 @@
 import Section from "../common/Section";
 import { Container, Typography, Box } from "@mui/material";
 import Subtitle from "../common/Subtitle";
-import {UniversitySearch} from "../common/UniversitySearch";
+import { UniversitySearch } from "../common/UniversitySearch";
 
 const SearchHero = () => {
-
   return (
     <Section bgcolor="grayscale.light" id="title-section">
-        <Typography variant="h2" component="h1" sx={{ mb: 2 }}>
-          Search Colleges
-        </Typography>
+      <Typography variant="h2" component="h1" sx={{ mb: 2 }}>
+        Search Colleges
+      </Typography>
 
-        {/* Left align subtitle and remove horizontal centering */}
-        <Subtitle sx={{ mx: 0, mb: 1}}> 
-            Find universities that support your unique identity and needs.
-        </Subtitle>
-        <UniversitySearch/>
+      {/* Left align subtitle and remove horizontal centering */}
+      <Subtitle sx={{ mx: 0, mb: 1 }}>
+        Find universities that support your unique identity and needs.
+      </Subtitle>
+      <UniversitySearch />
     </Section>
   );
 };
