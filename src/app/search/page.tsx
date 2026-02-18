d66a5ae19132af3a498f8b60d459b90ca6f9f79d
jackiecosa <jackie@benincosa.com>
2026-02-17 16:36:53 -0800

    add alligator braces to NotFound component

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,7 +9,7 @@ export default function SearchPage() {
   const isSearchEnabled = process.env.NEXT_PUBLIC_SHOW_SEARCH_PAGE === "true";
   // If the flag is false, show a NotFound page instead of the search UI
   if (!isSearchEnabled) {
-    return NotFound;
+    return <NotFound />;
   }
 
   return (
