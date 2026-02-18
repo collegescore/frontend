4b7e6ba61ee469f3219422254adac5e7a5fa9af9
jackiecosa <jackie@benincosa.com>
2026-02-17 16:28:42 -0800

    show not found if flag is on

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,8 +3,15 @@
 import React from "react";
 import { Container, Box, Typography } from "@mui/material";
 import { UniversitySearch } from "@/components/common/UniversitySearch";
+import NotFound from "../not-found";
 
 export default function SearchPage() {
+  const isSearchEnabled = process.env.NEXT_PUBLIC_SHOW_SEARCH_PAGE === 'true';
+  // If the flag is false, show a NotFound page instead of the search UI
+  if (!isSearchEnabled) {
+    return NotFound;
+  };
+
   return (
     <Container id="search-page" sx={{ mt: 4, mb: 8 }}>
       <UniversitySearch />
