99f296d3b983db4ac3e38f019ffc057020c5f13b
jackiecosa <jackie@benincosa.com>
2026-02-20 14:22:37 -0800

    make some college fields optional

--- src/types/college.ts
+++ src/types/college.ts
@@ -8,8 +8,9 @@ export interface College {
   name: string;
   city: string;
   state: string; //note that this is the state abbreviation, e.g. "CA" for California
-  a11y_overall: number;
-  safety_overall: number;
-  inclusivity_overall: number;
-  num_reviews: number;
+  // below are optional fields that may not be present in all contexts.
+  a11y_overall?: number;
+  safety_overall?: number;
+  inclusivity_overall?: number;
+  num_reviews?: number;
 }
