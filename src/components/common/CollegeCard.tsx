d2fbaa9038437d0f6adaf15d4e121c6450e91112
jackiecosa <jackie@benincosa.com>
2026-02-20 12:53:59 -0800

    create college car interface

---
+++ src/components/common/CollegeCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface CollegeCardProps {
+  name: string;
+  city: string;
+  state: string;
+  accessibility: number;
+  safety: number;
+  inclusivity: number;
+  reviewCount: number;
+}
+
