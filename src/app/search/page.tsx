a8f5b5fcebdf858f120e65a938b22a74f08d64e3
jackiecosa <jackie@benincosa.com>
2026-02-20 14:42:21 -0800

    remove from search page. will add later in separate PR where I actually make a grid of them.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,25 +4,11 @@ import React from "react";
 import { Container, Box, Typography } from "@mui/material";
 import { UniversitySearch } from "@/components/common/UniversitySearch";
 import SearchHero from "@/components/search/Hero";
-import CollegeCard from "@/components/common/CollegeCard";
 
 export default function SearchPage() {
   return (
     <Container id="search-page" sx={{ mt: 4, mb: 8 }}>
       <SearchHero />
-      <CollegeCard
-        college={{
-          id: 1,
-          slug: "example-university-example-city-ex",
-          name: "Example University",
-          city: "Example City",
-          state: "EX",
-          a11y_overall: 4.2,
-          safety_overall: 3.8,
-          inclusivity_overall: 4.5,
-          num_reviews: 128,
-        }}
-      />
     </Container>
   );
 }
