13f25aacd1c364e01986578fdbfbcb8e6872fb21
jackiecosa <jackie@benincosa.com>
2026-02-22 14:39:53 -0800

    add route to api.ts

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,17 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Box, Typography } from "@mui/material";
 import { UniversitySearch } from "@/components/common/UniversitySearch";
 import NotFound from "../not-found";
 import { FEATURE_FLAGS } from "@/config/flag";
 import SearchHero from "@/components/search/Hero";
+import CollegeCard from "@/components/common/CollegeCard";
+import { College } from "@/types/college";
+
 
 export default function SearchPage() {
   // If the flag is false, show a NotFound page instead of the search UI
   if (!FEATURE_FLAGS.isSearchEnabled) {
     return <NotFound />;
   }
+
+  const [colleges, setColleges] = useState<College[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTopColleges = async () => {
+      try {
+        // Points to your FastAPI v0 endpoint
+        const response = await fetch("");
+        if (!response.ok) throw new Error("Failed to fetch");
+        const data = await response.json();
+        setColleges(data);
+      } catch (error) {
+        console.error("Error loading colleges:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, []);
 
   return (
     <Container id="search-page" sx={{ mt: 4, mb: 8 }}>
