732929df4b5caa67bc81b6c8d62764a4cc240755
Danielle <dmaddo@uw.edu>
2026-02-26 15:58:41 -0800

    add flag to be able to see college without pulling data from backend

--- src/config/flag.ts
+++ src/config/flag.ts
@@ -3,5 +3,6 @@
  */
 export const FEATURE_FLAGS = {
   isSearchEnabled: process.env.NEXT_PUBLIC_SHOW_SEARCH_PAGE === "true",
+  isCollegePageBackendEnabled: process.env.NEXT_PUBLIC_COLLEGE_PAGE_BACKEND === "true",
   // more flags can be added here in the future as needed
 } as const;
