7805716c443499f2d1e7d8056311b8c10bf30d6a
Danielle <dmaddo@uw.edu>
2026-02-27 15:34:54 -0800

    remove invalid comment

--- src/components/college/SummaryCard.tsx
+++ src/components/college/SummaryCard.tsx
@@ -27,8 +27,7 @@ const SummaryCard = ({ title, content }: SummaryCardProps) => {
       <Box sx={{ display: "flex", alignItems: "center", gap: 1, mt: 2 }}>
         <Chip label="Thing 1" color="primary" variant="square" />
         <Chip label="Thing 2" color="primary" variant="square" />
-        <Chip label="Thing 3" color="primary" variant="square" />{" "}
-        {/* This will use the default MUI Chip styling */}
+        <Chip label="Thing 3" color="primary" variant="square" />
       </Box>
     </Card>
   );
