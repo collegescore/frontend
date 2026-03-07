204bfedf965ce4df9099f69fe4937b010cd4bca4
Danielle <dmaddo@uw.edu>
2026-03-07 15:30:57 -0800

    add review feature flags for features not yet developed

--- src/config/flag.ts
+++ src/config/flag.ts
@@ -5,5 +5,7 @@ export const FEATURE_FLAGS = {
   isSearchEnabled: process.env.NEXT_PUBLIC_SHOW_SEARCH_PAGE === "true",
   isCollegePageBackendEnabled:
     process.env.NEXT_PUBLIC_COLLEGE_PAGE_BACKEND === "true",
+  isReviewSortEnabled: process.env.NEXT_PUBLIC_REVIEW_SORT === "true",
+  isReviewSummariesEnabled: process.env.NEXT_PUBLIC_SHOW_SUMMARIES === "true",
   // more flags can be added here in the future as needed
 } as const;
