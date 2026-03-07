c37252c9b2a8df456312381ffe616445b7db6d04
Danielle <dmaddo@uw.edu>
2026-03-07 13:59:15 -0800

    align with whats being passed from backend

--- src/types/review_entry.ts
+++ src/types/review_entry.ts
@@ -1,11 +1,13 @@
 /*Review entry type definition, can be expanded as needed to include more fields such as review text, date, etc.*/
+import { UUID } from "crypto";
 import { Ratings } from "./ratings";
 export interface ReviewEntry {
+  id: UUID;
   review_date: string;
-  identity_chips: string[]; // e.g. ["wheelchair user", "LGBTQ+"]
+  identities: string[]; // e.g. ["wheelchair user", "LGBTQ+"]
   share_accommodations_text: string;
   share_positive_text: string;
   share_challenges_text: string;
-  share_community_groups_text: string;
+  share_groups_text: string;
   ratings: Ratings;
 }
