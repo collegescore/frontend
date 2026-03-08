48aeaa5cefc621ff8233755107969594c611c22b
jackiecosa <jackie@benincosa.com>
2026-03-08 16:36:23 -0700

    add email validation documentation

--- src/lib/emailValidation.ts
+++ src/lib/emailValidation.ts
@@ -1,4 +1,11 @@
-// Copied exactly from Python EmailService pattern. if changing logic here, also change there.
+// logic copied from Backend email_service pattern. if changing logic here, also change there.
+
+/* email: usernamename@address
+ *      ^(?!\.)(?!.*\.\.)[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+ allowed characters in username, prevents consecutive periods
+ *      @ mandatory separator
+ *      [A-Za-z0-9.-]+ domain name
+ *      \.[A-Za-z]{2,7} extension like .com, .org, .uk
+*/
 export const EMAIL_REGEX =
   /^(?!\.)(?!.*\.\.)[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,7}$/;
 
